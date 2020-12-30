07635967a07a02c355702998d59d30f0461afac8
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-12-30 19:35:05 +0900

    test: NewCmdVersion

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestBuildEnvsString(t *testing.T) {
+	command := "tss version"
 	want := `TSS_BUILD_VERSION="0.1.2-development"
 TSS_BUILD_COMMIT="aaeb6725631dcff02055855ee263ef5f45ed1eea-development"
 TSS_BUILD_DATE="2020-12-28T11:01:32Z-development"
@@ -12,8 +16,17 @@ TSS_BUILD_GOOS="darwin"
 TSS_BUILD_GOARCH="amd64"
 TSS_BUILD_GOVERSION="go1.15.6"
 `
-	out := GetBuildEnvsString("darwin", "amd64", "go1.15.6")
+
+	buf := new(bytes.Buffer)
+	cmd := NewCmdVersion()
+	cmd.SetOutput(buf)
+	cmdArgs := strings.Split(command, " ")
+	fmt.Printf("cmdArgs %+v\n", cmdArgs)
+	cmd.SetArgs(cmdArgs[1:])
+	cmd.Execute()
+	out := buf.String()
+
 	if out != want {
-		t.Errorf("BuildEnvString(); want \n%s", want)
+		t.Errorf("tss version: want \n%s", want)
 	}
 }
